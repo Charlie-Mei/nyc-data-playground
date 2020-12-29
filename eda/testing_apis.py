bf5fa3b3455e79c4ee2ca8ac58c3fc172c64fcd3
Charlie-Mei <charlie.mei@outlook.com>
2020-12-29 12:04:28 -0500

    decomp + ses modeling

--- eda/testing_apis.py
+++ eda/testing_apis.py
@@ -2,14 +2,19 @@ import json
 import os
 import requests
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
+
+from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 # Load credentials
 from dotenv import load_dotenv
 load_dotenv(os.path.join("C:\\Github\\credentials.env"))
 EIA_API_TOKEN = os.getenv("EIA_API_TOKEN")
-
 
 # Test request
 response = requests.get(f"http://api.eia.gov/series/?api_key={EIA_API_TOKEN}&series_id=ELEC.GEN.ALL-NY-99.M")
@@ -26,3 +31,18 @@ elec_all_gen.columns = ["date", "net_generation"]
 elec_all_gen.date = pd.to_datetime(elec_all_gen.date, format="%Y%m")
 elec_all_gen.dtypes
 elec_all_gen = elec_all_gen.sort_values(["date"]).reset_index()
+
+sns.relplot(data=elec_all_gen, x="date", y="net_generation", kind="line")
+plt.show()
+
+# Additive seasonal decomposition
+decomp = elec_all_gen.net_generation
+decomp_results = seasonal_decompose(decomp, model="additive", period=12)
+decomp_results.plot()
+plt.show()
+
+# Multiplicative seasonal decomposition
+decomp = elec_all_gen.net_generation
+decomp_results = seasonal_decompose(decomp, model="multiplicative", period=12)
+decomp_results.plot()
+plt.show()
