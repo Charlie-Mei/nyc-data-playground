71ba0b23c607aa6e39df5aef3d5fe63f550de5f4
Charlie-Mei <charlie.mei@outlook.com>
2021-08-12 23:35:48 -0400

    tidy repo

--- eda/testing_apis.py
+++ eda/testing_apis.py
@@ -7,10 +7,6 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.tsa.holtwinters import ExponentialSmoothing
-from statsmodels.tsa.seasonal import seasonal_decompose
-
 # Load credentials
 from dotenv import load_dotenv
 load_dotenv(os.path.join("C:\\Github\\credentials.env"))
@@ -33,16 +29,4 @@ elec_all_gen.dtypes
 elec_all_gen = elec_all_gen.sort_values(["date"]).reset_index()
 
 sns.relplot(data=elec_all_gen, x="date", y="net_generation", kind="line")
-plt.show()
-
-# Additive seasonal decomposition
-decomp = elec_all_gen.net_generation
-decomp_results = seasonal_decompose(decomp, model="additive", period=12)
-decomp_results.plot()
-plt.show()
-
-# Multiplicative seasonal decomposition
-decomp = elec_all_gen.net_generation
-decomp_results = seasonal_decompose(decomp, model="multiplicative", period=12)
-decomp_results.plot()
 plt.show()
